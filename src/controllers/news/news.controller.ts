531f38890cb2fb040ca89cfae625f3436a35b803
doncolmi <matathresh1740@naver.com>
2020-08-23 14:27:18 +0900

    fix conflict

--- src/controllers/news/news.controller.ts
+++ src/controllers/news/news.controller.ts
@@ -11,55 +11,44 @@ class NewsController implements Controller {
   constructor() {
     this.initializeRoutes();
   }
-
-<<<<<<< HEAD
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.getNewsListAll);
+    this.router.get(
+      `${this.path}`,
+      this.getFirstNewsListAll,
+      this.getNewsListAll
+    );
   }
+
+  private getFirstNewsListAll = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (req.query.date) {
+      next();
+      return;
+    }
+    this.news
+      .find({})
+      .sort({ createdDate: -1 })
+      .limit(10)
+      .then((result: any) => res.json(result))
+      .catch((err: Error) => next(err));
+  };
 
   private getNewsListAll = (
     req: Request,
     res: Response,
     next: NextFunction
   ) => {
-    const page: number = +req.query.page!;
-    console.log(page, " 입니다.");
+    const date: string = req.query.date as string;
     this.news
-      .find({})
+      .find({ createdDate: { $lt: new Date(date) } })
       .sort({ createdDate: -1 })
-      .skip(page * 10)
       .limit(10)
       .then((result: any) => res.json(result))
       .catch((err: Error) => next(err));
   };
-=======
-    private initializeRoutes() {
-        this.router.get(`${this.path}`, this.getFirstNewsListAll, this.getNewsListAll);
-    }
-
-    private getFirstNewsListAll = (req: Request, res: Response, next: NextFunction) => {
-        if(req.query.date) {
-            next();
-            return;
-        }
-        this.news
-        .find({})
-        .sort({ createdDate: -1})
-        .limit(10)
-        .then(((result: any) => res.json(result)))
-        .catch((err: Error) => next(err));
-    }
-
-    private getNewsListAll = (req: Request, res: Response, next: NextFunction) => {
-        const date: string = req.query.date as string;
-        this.news
-        .find({ "createdDate": { "$lt" : new Date(date) } })
-        .sort({ createdDate: -1})
-        .limit(10)
-        .then(((result: any) => res.json(result)))
-        .catch((err: Error) => next(err));
-    }
->>>>>>> a52f3fdb0c79c6822892dbd6bfbe16b92a2f3db7
 }
 
 export default NewsController;
