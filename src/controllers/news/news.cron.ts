6a542d476d5887a5df9b837a853bf511227a1d6d
doncolmi <matathresh1740@naver.com>
2020-09-04 02:10:59 +0900

    fix cron about newsId

--- src/controllers/news/news.cron.ts
+++ src/controllers/news/news.cron.ts
@@ -2,6 +2,7 @@ import { schedule } from "node-cron";
 
 import newsService from "./news.service";
 
-export const test = schedule("0 */3 * * * *", async () => {
+export const test = schedule("30 */3 * * * *", async () => {
+  console.log("실행");
   new newsService().doCrawling();
 });
